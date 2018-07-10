f9717b78c4445dd7874ed8a76268428a9a034819
hyhweb <461342854@qq.com>
2018-07-10 20:59:17 +0800

    code

---
+++ pages/hotsale/list/index.js
@@ -0,0 +1,127 @@
+// pages/hotsale/list/index.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[],
+    tabs: ["最新", "最热"],
+    activeIndex: 0,
+    sliderOffset:0,
+    pageF:1,
+    pageS:1
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+    if (this.data.activeIndex == 0){
+      this.getList({ desc: 'new' })
+    } else if (this.data.activeIndex == 1){
+      this.getList({ desc: 'hot' })
+    }
+  },
+  getList: function (param) {
+    var param = {
+      url: '/product/pageList',
+      method: 'post',
+      data:param,
+      success: (res) => {
+        if (res.success) {
+          var list = this.data.list;
+          this.setData({
+            list: list.concat(res.result.list)
+          })
+        }
+      }
+    }
+    app.request(param);
+  },
+  loadMore:function(){
+    var param = {pageSize:10};
+    if (this.data.activeIndex == 0) {
+      param.desc ="new";
+      param.page = this.data.pageF+1;
+      this.setData({
+        pageF: param.page
+      })
+      this.getList(param);
+    } else if (this.data.activeIndex == 1) {
+      param.desc = "hot";
+      param.page = this.data.pageS + 1;
+      this.setData({
+        pageS: param.page
+      })
+      this.getList(param);
+    }
+    
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getList({desc:'new'})
+    // var that = this;
+    // wx.getSystemInfo({
+    //   success: function (res) {
+    //     that.setData({
+    //       sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+    //       sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+    //     });
+    //     console.log(res,'res')
+    //   }
+    // });
+  },
+ 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
